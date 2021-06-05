9536c198cac6b11cd5c4761383dc83531ce70a73
ElideZavala <zavalavinagreelide@gmail.com>
2021-06-04 19:30:06 -0600

    efectos de transición

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Hola Mundo Hermss');
+const labels = document.querySelectorAll('.form-control label');
+
+labels.forEach(label => {
+	label.innerHTML = label.innerText // <== Para cada letra del label
+		.split('') // <== Cada letra la separamos por un espacio vacio	
+		.map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`) // <== Creamos un nuevo elemento con las letras separadas. 
+		.join(''); // <== Unimos los elementos 
+
+})
